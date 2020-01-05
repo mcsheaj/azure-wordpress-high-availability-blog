d5d0b60d7ab077c3e32b585fb19e12522d2c7515
mcsheaj <joseph.mcshea@gmail.com>
2020-01-05 18:46:23 -0500

    initial commit

---
+++ jumpbox-scale-set/scripts/bootstrap-jumpbox.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -xe
+
+# Resync the package index files and upgrade all installed packages
+apt clean
+apt-get -y upgrade
+
+# Flush IP tables
+iptables -F
+
+# Configure IP tables to allow inbound SSH, DHCP, and traffic for established outbound connections
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p udp --dport 67:68 --sport 67:68 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -A INPUT -j DROP
+
+# Persist IP tables configuration
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+apt-get install iptables-persistent
+
+# Install fail2ban
+apt-get -y install fail2ban
+
+# Configure fail2ban
+cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+sed -i "s/maxretry = 5/maxretry = 3/" /etc/fail2ban/jail.local
+sed -i "s/^\[sshd\]/[sshd]\nenabled=true/" /etc/fail2ban/jail.local
+
+# Enable fail2ban to start as a service and start it now
+systemctl enable fail2ban
+systemctl start fail2ban
