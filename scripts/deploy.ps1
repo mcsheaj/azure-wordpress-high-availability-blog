9697349aadbad3d578803e2633e02fd824be12a2
mcsheaj <joseph.mcshea@gmail.com>
2020-01-06 20:17:05 -0500

    updated naming convention

---
+++ scripts/deploy.ps1
@@ -0,0 +1,6 @@
+# vnet deployment
+New-AzureRmResourceGroupDeployment -Name vnet-deployment-prod-westus2 -ResourceGroupName rg-wp-prod-westus2-001 -TemplateUri https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/two-tier-vnet/azuredeploy.json -TemplateParameterUri https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/two-tier-vnet/azuredeploy.parameters.json -Mode Incremental
+
+# jumpbox deployment
+New-AzureRmResourceGroupDeployment -Name vnet-deployment-prod-westus2 -ResourceGroupName rg-wp-prod-westus2-001 -TemplateUri https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/two-tier-vnet/azuredeploy.json -TemplateParameterUri https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/two-tier-vnet/azuredeploy.parameters.json -Mode Incremental
+
