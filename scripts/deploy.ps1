23fc3fe74655cd3df3f5b9c66b5db8e111207f1f
mcsheaj <joseph.mcshea@gmail.com>
2020-01-07 02:10:20 -0500

    normalized deployment names

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,10 +1,10 @@
 # jumpbox deployment local params file
 wget -O azuredeploy.json --no-cache https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/two-tier-vnet/azuredeploy.json
 wget -O azuredeploy.parameters.json --no-cache https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/two-tier-vnet/azuredeploy.parameters.json
-New-AzureRmResourceGroupDeployment -Name vnet-deployment-prod-westus2 -ResourceGroupName rg-wp-prod-westus2-001 -TemplateFile $HOME/azuredeploy.json -TemplateParameterFile $HOME/azuredeploy.parameters.json -Mode Incremental -RollbackToLastDeployment
+New-AzureRmResourceGroupDeployment -Name vnet-deployment-wp-prod-westus2 -ResourceGroupName rg-wp-prod-westus2-001 -TemplateFile $HOME/azuredeploy.json -TemplateParameterFile $HOME/azuredeploy.parameters.json -Mode Incremental -RollbackToLastDeployment
 
 # jumpbox deployment local params file
 wget -O azuredeploy.json --no-cache https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/jumpbox-scale-set/azuredeploy.json
 wget -O azuredeploy.parameters.json --no-cache https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/jumpbox-scale-set/azuredeploy.parameters.json
-New-AzureRmResourceGroupDeployment -Name vmss-deployment-wp-jumpbox-prod-westus2 -ResourceGroupName rg-wp-prod-westus2-001 -TemplateFile $HOME/azuredeploy.json -TemplateParameterFile $HOME/azuredeploy.parameters.json -Mode Incremental -RollbackToLastDeployment
+New-AzureRmResourceGroupDeployment -Name vmss-deployment-jumpbox-wp-prod-westus2 -ResourceGroupName rg-wp-prod-westus2-001 -TemplateFile $HOME/azuredeploy.json -TemplateParameterFile $HOME/azuredeploy.parameters.json -Mode Incremental -RollbackToLastDeployment
 
