8455a8109276854504759fa69568c7940efb16f7
mcsheaj <joseph.mcshea@gmail.com>
2020-01-06 20:58:43 -0500

    fixed subneet parameter

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -4,3 +4,9 @@ New-AzureRmResourceGroupDeployment -Name vnet-deployment-prod-westus2 -ResourceG
 # jumpbox deployment
 New-AzureRmResourceGroupDeployment -Name vmss-deployment-wp-jumpbox-prod-westus2 -ResourceGroupName rg-wp-prod-westus2-001 -TemplateUri https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/jumpbox-scale-set/azuredeploy.json -TemplateParameterUri https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/jumpbox-scale-set/azuredeploy.parameters.json -Mode Incremental
 
+# jumpbox deployment local params file
+wget -O azuredeploy.json --no-cache https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/jumpbox-scale-set/azuredeploy.json
+wget -O azuredeploy.parameters.json --no-cache https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/jumpbox-scale-set/azuredeploy.parameters.json
+New-AzureRmResourceGroupDeployment -Name vmss-deployment-wp-jumpbox-prod-westus2 -ResourceGroupName rg-wp-prod-westus2-001 -TemplateFile $HOME/azuredeploy.json -TemplateParameterFile $HOME/azuredeploy.parameters.json -Mode Incremental
+
+RollbackToLastDeployment
