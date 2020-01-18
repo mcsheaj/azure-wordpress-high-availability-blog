fa212abef656009cdb92ae121d246c3cd33a7536
mcsheaj <joseph.mcshea@gmail.com>
2020-01-18 14:54:09 -0500

    .

--- _deploy/deploy.sh
+++ _deploy/deploy.sh
@@ -8,6 +8,10 @@ wget -O "azuredeploy.parameters.json" --no-cache "https://raw.githubusercontent.
 wget -O "azuredeploy.json" --no-cache "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/stor-privatelink/azuredeploy.json"
 wget -O "azuredeploy.parameters.json" --no-cache "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/stor-privatelink/azuredeploy.parameters.json"
 
+# download the template and parameter files for the Privatelink DNS address records with no caching
+wget -O "azuredeploy.json" --no-cache "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/dns-arecords/azuredeploy.json"
+wget -O "azuredeploy.parameters.json" --no-cache "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/dns-arecords/azuredeploy.parameters.json"
+
 # download the template and parameter files for the JumpBox scale set with no caching
 wget -O "azuredeploy.json" --no-cache "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/vmss-jumpbox/azuredeploy.json"
 wget -O "azuredeploy.parameters.json" --no-cache "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/vmss-jumpbox/azuredeploy.parameters.json"
@@ -20,8 +24,11 @@ az group deployment create --name "vnet-deployment-wp-test-eastus" --resource-gr
 az group deployment create --name "ep-stor-deployment-wp-prod-westus2" --resource-group "rg-wp-prod-westus2-001" --template-file "azuredeploy.json" --parameters @"azuredeploy.parameters.json" --handle-extended-json-format
 az group deployment create --name "ep-stor-deployment-wp-test-eastus" --resource-group "rg-wp-test-eastus-001" --template-file "azuredeploy.json" --parameters @"azuredeploy.parameters.json" --handle-extended-json-format
 
+# deploy DNS address records for the PrivateLink endpoings
+az group deployment create --name "dns-arecords-deployment-wp-test-eastus" --resource-group "rg-wp-test-eastus-001" --template-file "azuredeploy.json" --parameters @"azuredeploy.parameters.json"
+
 # deploy the JumpBox scal set
 az group deployment create --name "vmss-deployment-jumpbox-wp-prod-westus2" --resource-group "rg-wp-prod-westus2-001" --template-file "azuredeploy.json" --parameters @"azuredeploy.parameters.json" --handle-extended-json-format
 az group deployment create --name "vmss-deployment-jumpbox-wp-test-eastus" --resource-group "rg-wp-test-eastus-001" --template-file "azuredeploy.json" --parameters @"azuredeploy.parameters.json" --handle-extended-json-format
 
-az resource list --resource-group rg-wp-prod-westus2-001 --resource-type Microsoft.Network/privateEndpoints
+az resource show --resource-group rg-wp-test-eastus-001 --resource-type Microsoft.Network/privateEndpoints --name ep-stor-subnet-public-vnet-wp-test-eastus
