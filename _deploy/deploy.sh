f60a9d11537e7e09aad6de1f1508ef30e487e479
mcsheaj <joseph.mcshea@gmail.com>
2020-01-18 22:06:28 -0500

    .

--- _deploy/deploy.sh
+++ _deploy/deploy.sh
@@ -16,6 +16,10 @@ wget -O "azuredeploy.parameters.json" --no-cache "https://raw.githubusercontent.
 wget -O "azuredeploy.json" --no-cache "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/vmss-jumpbox/azuredeploy.json"
 wget -O "azuredeploy.parameters.json" --no-cache "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/vmss-jumpbox/azuredeploy.parameters.json"
 
+# download the template and parameter files for the JumpBox scale set with no caching
+wget -O "azuredeploy.json" --no-cache "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/vmss-apache/azuredeploy.json"
+wget -O "azuredeploy.parameters.json" --no-cache "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/vmss-apache/azuredeploy.parameters.json"
+
 # deploy the VNet
 az group deployment create --name "vnet-deployment-wp-prod-westus2" --resource-group "rg-wp-prod-westus2-001" --template-file "azuredeploy.json" --parameters @"azuredeploy.parameters.json" --handle-extended-json-format
 az group deployment create --name "vnet-deployment-wp-test-eastus" --resource-group "rg-wp-test-eastus-001" --template-file "azuredeploy.json" --parameters @"azuredeploy.parameters.json" --handle-extended-json-format
@@ -27,8 +31,12 @@ az group deployment create --name "ep-stor-deployment-wp-test-eastus" --resource
 # deploy DNS address records for the PrivateLink endpoings
 az group deployment create --name "dns-arecords-deployment-wp-test-eastus" --resource-group "rg-wp-test-eastus-001" --template-file "azuredeploy.json" --parameters @"azuredeploy.parameters.json"
 
-# deploy the JumpBox scal set
+# deploy the JumpBox scale set
 az group deployment create --name "vmss-deployment-jumpbox-wp-prod-westus2" --resource-group "rg-wp-prod-westus2-001" --template-file "azuredeploy.json" --parameters @"azuredeploy.parameters.json" --handle-extended-json-format
 az group deployment create --name "vmss-deployment-jumpbox-wp-test-eastus" --resource-group "rg-wp-test-eastus-001" --template-file "azuredeploy.json" --parameters @"azuredeploy.parameters.json" --handle-extended-json-format
+
+# deploy the Apache scale set
+az group deployment create --name "vmss-deployment-apache-wp-prod-westus2" --resource-group "rg-wp-prod-westus2-001" --template-file "azuredeploy.json" --parameters @"azuredeploy.parameters.json" --handle-extended-json-format
+az group deployment create --name "vmss-deployment-apache-wp-test-eastus" --resource-group "rg-wp-test-eastus-001" --template-file "azuredeploy.json" --parameters @"azuredeploy.parameters.json" --handle-extended-json-format
 
 az resource show --resource-group rg-wp-test-eastus-001 --resource-type Microsoft.Network/privateEndpoints --name ep-stor-subnet-public-vnet-wp-test-eastus
