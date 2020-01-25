681599e700b5f0ad0f107ef3ed0db4611679d3ff
mcsheaj <joseph.mcshea@gmail.com>
2020-01-25 09:53:15 -0500

    .

--- _deploy/deploy.sh
+++ _deploy/deploy.sh
@@ -22,6 +22,51 @@ az group deployment create \
     --template-file "azuredeploy.json" \
     --parameters @"azuredeploy.parameters.json"
 
+# download the template and parameter files for the Storage and Privatelink with no caching
+wget \
+    --output-document="azuredeploy.json" \
+    --no-cache \
+    "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/stor-privatelink/azuredeploy.json"
+wget \
+    --output-document="azuredeploy.parameters.json" \
+    --no-cache \
+    "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/stor-privatelink/azuredeploy.parameters.json"
+
+# deploy Storage and PrivateLink endpoints and private DNS zone for PrivateLink
+az group deployment create \
+    --name "ep-stor-deployment-wp-prod-westus2" \
+    --resource-group "rg-wp-prod-westus2-001" \
+    --template-file "azuredeploy.json" \
+    --parameters @"azuredeploy.parameters.json"
+az group deployment create \
+    --name "ep-stor-deployment-wp-test-eastus" \
+    --resource-group "rg-wp-test-eastus-001" \
+    --template-file "azuredeploy.json" \
+    --parameters @"azuredeploy.parameters.json"
+
+# download the template and parameter files for the Privatelink DNS address records with no caching
+wget \
+    --output-document="azuredeploy.json" \
+    --no-cache \
+    "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/dns-arecords/azuredeploy.json"
+wget \
+    --output-document="azuredeploy.parameters.json" \
+    --no-cache \
+    "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/dns-arecords/azuredeploy.parameters.json"
+
+# deploy DNS address records for the PrivateLink endpoings
+az group deployment create \
+    --name "dns-arecords-deployment-wp-test-westus2" \
+    --resource-group "rg-wp-test-westus2-001" \
+    --template-file "azuredeploy.json" \
+    --parameters @"azuredeploy.parameters.json"
+az group deployment create \
+    --name "dns-arecords-deployment-wp-test-eastus" \
+    --resource-group "rg-wp-test-eastus-001" \
+    --template-file "azuredeploy.json" \
+    --parameters @"azuredeploy.parameters.json"
+
+
 # download the template and parameter files for the JumpBox scale set with no caching
 wget \
     --output-document="azuredeploy.json" \
@@ -93,46 +138,3 @@ az group deployment create \
     --handle-extended-json-format
 
 
-# download the template and parameter files for the Storage and Privatelink with no caching
-wget \
-    --output-document="azuredeploy.json" \
-    --no-cache \
-    "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/stor-privatelink/azuredeploy.json"
-wget \
-    --output-document="azuredeploy.parameters.json" \
-    --no-cache \
-    "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/stor-privatelink/azuredeploy.parameters.json"
-
-# deploy Storage and PrivateLink endpoints and private DNS zone for PrivateLink
-az group deployment create \
-    --name "ep-stor-deployment-wp-prod-westus2" \
-    --resource-group "rg-wp-prod-westus2-001" \
-    --template-file "azuredeploy.json" \
-    --parameters @"azuredeploy.parameters.json"
-az group deployment create \
-    --name "ep-stor-deployment-wp-test-eastus" \
-    --resource-group "rg-wp-test-eastus-001" \
-    --template-file "azuredeploy.json" \
-    --parameters @"azuredeploy.parameters.json"
-
-# download the template and parameter files for the Privatelink DNS address records with no caching
-wget \
-    --output-document="azuredeploy.json" \
-    --no-cache \
-    "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/dns-arecords/azuredeploy.json"
-wget \
-    --output-document="azuredeploy.parameters.json" \
-    --no-cache \
-    "https://raw.githubusercontent.com/mcsheaj/azure-wordpress-high-availability-blog/master/dns-arecords/azuredeploy.parameters.json"
-
-# deploy DNS address records for the PrivateLink endpoings
-az group deployment create \
-    --name "dns-arecords-deployment-wp-test-westus2" \
-    --resource-group "rg-wp-test-westus2-001" \
-    --template-file "azuredeploy.json" \
-    --parameters @"azuredeploy.parameters.json"
-az group deployment create \
-    --name "dns-arecords-deployment-wp-test-eastus" \
-    --resource-group "rg-wp-test-eastus-001" \
-    --template-file "azuredeploy.json" \
-    --parameters @"azuredeploy.parameters.json"
